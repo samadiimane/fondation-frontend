4b42b29a15e1bd5038fe3ada2d4ffe49f2a0378e
imane <imanesamadi21@gmail.com>
2025-10-18 22:37:21 +0100

    Enable standalone Next.js build

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@ const withNextIntl = require('next-intl/plugin')('./src/i18n/request.js');
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false
+  reactStrictMode: false,
+  output: 'standalone'
 };
 
 module.exports = withNextIntl(nextConfig);
