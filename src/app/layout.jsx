27baf8a4f93107b100f1313cb337350e0a5573e4
imane <imanesamadi21@gmail.com>
2026-05-14 15:02:35 +0100

    perf: remove duplicate global asset hints

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -26,8 +26,6 @@ export default async function RootLayout({children}) {
           href='https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Outfit:wght@100..900&display=swap'
           rel='stylesheet'
         />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
         <link
           href='https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner:wght@400..700&family=Hubot+Sans:ital,wght@0,200..900;1,200..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
           rel='stylesheet'
@@ -42,7 +40,6 @@ export default async function RootLayout({children}) {
         <link rel='stylesheet' href='/assets/css/default-theme.css' id='switch-color' />
         <link rel='stylesheet' href='/assets/css/sticky-header.css' />
         <link rel='stylesheet' href='/assets/css/box-layout.css' />
-        <link rel='stylesheet' href='/assets/css/flag-icons.min.css' />
         <link rel='stylesheet' href='/assets/css/dark-mode.css' />
       </head>
       <body className={isRtl ? 'rtl' : undefined} suppressHydrationWarning>
