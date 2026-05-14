1587f7898ca9fd8dc378d1be295c2619057d3a39
imane <imanesamadi21@gmail.com>
2026-05-14 17:04:46 +0100

    perf: remove duplicate global asset hints

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,20 @@
 import {defaultLocale, isRtlLocale} from '@/i18n/config';
 import {getLocale} from 'next-intl/server';
+import {Caveat, Nunito} from 'next/font/google';
+
+const nunito = Nunito({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800', '900'],
+  display: 'swap',
+  variable: '--font-nunito'
+});
+
+const caveat = Caveat({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700'],
+  display: 'swap',
+  variable: '--font-caveat'
+});
 
 export const metadata = {
   title: 'AKT research foundation',
@@ -18,19 +33,13 @@ export default async function RootLayout({children}) {
   const isRtl = isRtlLocale(resolvedLocale);
 
   return (
-    <html lang={resolvedLocale} dir={isRtl ? 'rtl' : 'ltr'} suppressHydrationWarning>
+    <html
+      lang={resolvedLocale}
+      dir={isRtl ? 'rtl' : 'ltr'}
+      className={`${nunito.variable} ${caveat.variable}`}
+      suppressHydrationWarning
+    >
       <head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='true' />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Caveat:wght@400..700&family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Outfit:wght@100..900&display=swap'
-          rel='stylesheet'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Edu+VIC+WA+NT+Beginner:wght@400..700&family=Hubot+Sans:ital,wght@0,200..900;1,200..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap'
-          rel='stylesheet'
-        />
-
         <link rel='stylesheet' href='/assets/fonts/css/all.min.css' />
         <link rel='stylesheet' href='/assets/fonts/css/flag-icons.min.css' />
         <link rel='stylesheet' href='/assets/fonts/css/charifund.css' />
